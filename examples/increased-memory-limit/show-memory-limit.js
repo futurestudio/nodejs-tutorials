70fb240def580ace71c7da568b8edbae70ffcb71
Marcus Poehls <marcus.poehls@gmail.com>
2017-09-04 15:54:30 +0200

    refine node.js max-old-space-size code

--- examples/increased-memory-limit/show-memory-limit.js
+++ examples/increased-memory-limit/show-memory-limit.js
@@ -5,4 +5,6 @@ const v8 = require('v8')
 //console.log(v8.getHeapStatistics())
 
 const totalHeapSize = v8.getHeapStatistics().total_available_size
-console.log(`Total heap size (bytes) ${totalHeapSize}, (GB ~${(totalHeapSize / 1024 / 1024 / 1024).toFixed(2)})`)
+let totalHeapSizeInGB = (totalHeapSize / 1024 / 1024 / 1024).toFixed(2)
+
+console.log(`Total heap size (bytes) ${totalHeapSize}, (GB ~${totalHeapSizeInGB})`)
