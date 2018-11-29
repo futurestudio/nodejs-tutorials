971d0afbbafabd72e708894ed51eebe1e61a589c
Marcus Poehls <marcus.poehls@gmail.com>
2018-11-29 17:00:15 +0100

    add array merge example with lodash

--- examples/merge-array-alternating-values/index.js
+++ examples/merge-array-alternating-values/index.js
@@ -11,6 +11,21 @@ const combined =
       return result.concat(pair)
     })
 
+console.log('Vanilla JS:')
 console.log(combined);
 // -> [ 1, 'A', 2, 'B', 3, 'C', 4, 'D' ]
 
+
+// Alternatively with lodash
+const _ = require('lodash')
+
+const lodashCombined =
+  _
+    .zip(first, second)
+    .reduce((result, pair) => {
+      return result.concat(pair)
+    })
+
+console.log('with lodash:')
+console.log(lodashCombined);
+// -> [ 1, 'A', 2, 'B', 3, 'C', 4, 'D' ]
