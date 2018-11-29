709b6457c04b15e874a9b2714c5998f7df2fb3ff
Marcus Poehls <marcus.poehls@gmail.com>
2018-11-29 15:39:04 +0100

    run promises in sequence

---
+++ examples/promises-in-sequence/index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+async function waitingTask(time) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log(`finished: ${time}`)
+      resolve(time)
+    }, time)
+  })
+}
+
+async function runInSequence(promises) {
+  return promises.reduce(async (chain, timeout) => {
+    return [ ... (await chain), await waitingTask(timeout)]
+  }, Promise.resolve([]))
+}
+
+const waitingTimes = [10, 600, 200, 775, 125, 990]
+
+runInSequence(waitingTimes).then(result => {
+  console.log();
+  console.log('YES! All done. Here is the result:');
+  console.log(result)
+})
