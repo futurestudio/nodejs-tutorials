b9f64389ed0a91efb4508d7fbaa00d3bf1f611c0
Marcus Poehls <marcus.poehls@gmail.com>
2018-12-05 15:46:45 +0100

    minor refactoring

--- examples/promises-in-parallel/index.js
+++ examples/promises-in-parallel/index.js
@@ -3,17 +3,16 @@
 async function waitingTask(time) {
   return new Promise(resolve => {
     setTimeout(() => {
-      console.log(`finished: ${time}`)
+      console.log(`waited: ${time}ms`)
       resolve(time)
     }, time)
   })
 }
 
 async function run() {
-  const waitingTimes = [10, 600, 200, 775, 125, 990]
-  const promises = waitingTimes.map(time => waitingTask(time))
+  const waitingTimes = [10, 600, 200, 775, 125, 990].map(time => waitingTask(time))
 
-  await Promise.all(promises)
+  await Promise.all(waitingTimes)
 
   console.log();
   console.log('YES! All done. Here is the result:');
