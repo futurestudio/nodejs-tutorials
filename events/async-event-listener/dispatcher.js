76d75b04dcebb6aa1206b2a8bf06a1791a30921a
Marcus Poehls <marcus.poehls@gmail.com>
2019-05-20 22:58:44 +0200

    clock

--- events/async-event-listener/dispatcher.js
+++ events/async-event-listener/dispatcher.js
@@ -3,19 +3,19 @@
 const Event = require('./event')
 const Listener = require('./listener')
 
-const delivery = new Event()
+const clock = new Event()
 const handler = new Listener()
 
-delivery.on('start', async (...args) => {
+clock.on('start', async (...args) => {
   await handler.onStart(...args)
 })
 
-delivery.on('stop', async (...args) => {
+clock.on('stop', async (...args) => {
   await handler.onStop(...args)
 })
 
-delivery.on('tick', async (...args) => {
+clock.on('tick', async (...args) => {
   await handler.onTick(...args)
 })
 
-delivery.start()
+clock.start()
