f9340f3125149ce1c17db19c0484c00965fa98cf
Marcus Poehls <marcus.poehls@gmail.com>
2019-05-22 14:30:08 +0200

    refactor example

--- axios/download-files-with-progress/index.js
+++ axios/download-files-with-progress/index.js
@@ -5,31 +5,49 @@ const Path = require('path')
 const Axios = require('axios')
 const ProgressBar = require('progress')
 
-async function download () {
-  const url = 'https://unsplash.com/photos/P6uF0I_okfk/download?force=true'
-  const path = Path.resolve(__dirname, 'images', 'hills.jpg')
+class DownloadWithProgressBar {
+  constructor () {
+    this.source = 'https://unsplash.com/photos/P6uF0I_okfk/download?force=true'
+    this.destination = Path.resolve(__dirname, 'images', 'hills.jpg')
+  }
 
-  const response = await Axios({
-    url,
-    method: 'GET',
-    responseType: 'stream'
-  })
+  async start () {
+    console.log('Starting download')
 
-  response.data.pipe(Fs.createWriteStream(path))
+    const { data, headers } = await this._downloadFile()
+    const progressBar = this._initializeProgressBar({ totalLength: headers['content-length'] })
 
-  const progressBar = new ProgressBar('-> downloading [:bar] :percent :etas', {
-    width: 40,
-    complete: '=',
-    incomplete: ' ',
-    renderThrottle: 1,
-    total: parseInt(response.headers['content-length'])
-  })
+    return new Promise((resolve, reject) => {
+      data.pipe(this._destinationWriteStream())
 
-  return new Promise((resolve, reject) => {
-    response.data.on('data', (chunk) => progressBar.tick(chunk.length))
-    response.data.on('end', resolve)
-    response.data.on('error', reject)
-  })
+      data.on('data', (chunk) => progressBar.tick(chunk.length))
+      data.on('end', resolve)
+      data.on('error', reject)
+    })
+  }
+
+  async _downloadFile () {
+    return Axios({
+      url: this.source,
+      method: 'GET',
+      responseType: 'stream'
+    })
+  }
+
+  _initializeProgressBar ({ totalLength }) {
+    return new ProgressBar('-> downloading [:bar] :percent :etas', {
+      width: 40,
+      complete: '=',
+      incomplete: ' ',
+      renderThrottle: 1,
+      total: parseInt(totalLength)
+    })
+  }
+
+  _destinationWriteStream () {
+    return Fs.createWriteStream(this.destination)
+  }
 }
 
-download()
+const download = new DownloadWithProgressBar()
+download.start()
