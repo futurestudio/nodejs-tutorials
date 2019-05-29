a6cf339c51be0a91e5c4ed2c803a1bf04ab9a21f
Marcus Poehls <marcus.poehls@gmail.com>
2019-05-29 09:35:56 +0200

    add comments; refine example

--- filesystem/create-an-empty-file/index.js
+++ filesystem/create-an-empty-file/index.js
@@ -5,18 +5,33 @@ const Fs = require('fs-extra')
 run()
 
 async function run () {
-  const descriptor = touchSync('./touched.sync')
-  Fs.closeSync(descriptor)
+  touchSync('./touched.sync')
   console.log('created "touched.sync"')
 
   await touch('./async.touch')
   console.log('created "async.touch"')
 }
 
-function touchSync (file) {
-  return Fs.openSync(file, 'w')
+/**
+ * Creates an empty file at `path`. This method does not ensure to create
+ * missing directories along the path. It won’t override an existing
+ * file or modify the content. Use the "w" flag to override an
+ * existing file and its content.
+ *
+ * @param {String} path
+ *
+ */
+function touchSync (path) {
+  Fs.closeSync(Fs.openSync(path, 'a'))
 }
 
-async function touch (file) {
-  await Fs.ensureFile(file)
+/**
+ * Ensures that the file at `path` exists. It will also create
+ * missing directories along the path. If a file at `path`
+ * already exists, it won’t modify the content.
+ *
+ * @param {String} path
+ */
+async function touch (path) {
+  await Fs.ensureFile(path)
 }
