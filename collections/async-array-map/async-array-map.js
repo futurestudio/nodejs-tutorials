75efd92cda810da212a999663a765e2beb7e0668
Marcus Poehls <marcus.poehls@gmail.com>
2019-11-25 22:09:59 +0100

    minor refinements

--- collections/async-array-map/async-array-map.js
+++ collections/async-array-map/async-array-map.js
@@ -2,10 +2,6 @@
 
 const Axios = require('axios')
 
-async function map (array, callback) {
-  return Promise.all(array.map(callback))
-}
-
 async function githubDetailsFor (repo) {
   const response = await Axios({
     method: 'GET',
@@ -25,10 +21,12 @@ async function run () {
     { url: 'https://api.github.com/repos/futurestudio/android-tutorials-glide' }
   ]
 
+  const promises = repos.map(async (repo) => {
+    return githubDetailsFor(repo)
+  })
+
   console.log(
-    await map(repos, async (repo) => {
-      return githubDetailsFor(repo)
-    })
+    await Promise.all(promises)
   )
 }
 
